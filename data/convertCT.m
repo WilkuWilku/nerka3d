28e124b290967d45374c83c5fd5add66c74a1259
Krzysiek <kwilk@ittouch.com>
2021-01-31 15:24:53 +0100

    konwersja wielokrotnych skanów dla jednej warstwy, poprawiony skrypt matlab

--- data/convertCT.m
+++ data/convertCT.m
@@ -1,28 +1,47 @@
-% przykladowa konwersja z data/ct7b.mat -> data/output/ct7b_x.json, gdzie x to kolejna warstwa skanu
+% przykladowa konwersja z data/ct7b.mat -> data/output/ct7b_typ_x-y.json, gdzie
+% typ -> jaki obiekt przedstawia mapa - nerka / cancer
+% x -> numer warstwy
+% y -> numer skanu dla danego typu i warstwy
 
-input_file_name = 'ct2b.mat';
-output_file_name_base = extractBefore(input_file_name, '.mat');
+function convertCT(input_file_name)
+    fprintf('Processing file: %s\n', input_file_name); 
+    output_file_name_base = extractBefore(input_file_name, '.mat');
 
-load(input_file_name);
-result = {};
-result_index = 0;
+    load(input_file_name);
+    result = {};
 
-for i = 1:length(R)
-   if ~isempty(R{i})
-       for j = 1:length(R{i})
-           if ~isempty(R{i}{j})
-               for k=1:(length(R{i}{j})-1)
-                   if ~isempty(R{i}{j}{k}) && strcmp(R{i}{j}{k+1},'nerka')
-                       result = R{i}{j}{k};
-                       new_output_file_name = sprintf('%s_%d.json', output_file_name_base, result_index); 
-                       fprintf('Adding R{%d}{%d}{%d} to file %s\n', i, j, k, strcat('output/',new_output_file_name));
-                       output_file = fopen(strcat('output/',new_output_file_name), 'w');
-                       fprintf(output_file, jsonencode(result));
+    for i = 1:length(R)
+       if ~isempty(R{i})
+           kidney_layer_index = 0;
+           cancer_layer_index = 0;
+           
+           for j = 1:length(R{i})
+               if ~isempty(R{i}{j})
+                   for k=1:(length(R{i}{j})-1)
+                       % nerka
+                       if ~isempty(R{i}{j}{k}) && strcmp(R{i}{j}{k+1},'nerka')
+                           result = R{i}{j}{k};
+                           new_output_file_name = sprintf('%s_kidney_%d-%d.json', output_file_name_base, i, kidney_layer_index); 
+                           fprintf('Adding R{%d}{%d}{%d} to file %s\n', i, j, k, strcat('output/',new_output_file_name));
+                           output_file = fopen(strcat('output/',new_output_file_name), 'w');
+                           fprintf(output_file, jsonencode(result));
+                           kidney_layer_index = kidney_layer_index + 1;
+                       end
                        
-                       result_index = result_index + 1;
+                       % rak
+                       if ~isempty(R{i}{j}{k}) && strcmp(R{i}{j}{k+1},'Cancer')
+                           result = R{i}{j}{k};
+                           new_output_file_name = sprintf('%s_cancer_%d-%d.json', output_file_name_base, i, cancer_layer_index); 
+                           fprintf('Adding R{%d}{%d}{%d} to file %s\n', i, j, k, strcat('output/',new_output_file_name));
+                           output_file = fopen(strcat('output/',new_output_file_name), 'w');
+                           fprintf(output_file, jsonencode(result));
+                           cancer_layer_index = cancer_layer_index + 1;
+                       end
                    end
+                   
                end
            end
-       end
-   end    
-end    
+           
+       end    
+    end
+end
