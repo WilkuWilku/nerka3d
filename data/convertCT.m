20819dc21c92dcc47aaecbbe923485b632dbfba3
Krzysiek <kwilk@ittouch.com>
2021-03-28 17:35:49 +0200

    numerowanie warstw wg kolejności w pliku

--- data/convertCT.m
+++ data/convertCT.m
@@ -4,44 +4,50 @@
 % y -> numer skanu dla danego typu i warstwy
 
 function convertCT(input_file_name)
-    fprintf('Processing file: %s\n', input_file_name); 
+    fprintf('Processing file: %s\n', input_file_name);
     output_file_name_base = extractBefore(input_file_name, '.mat');
 
     load(input_file_name);
     result = {};
-
+    layer_number = 0;
     for i = 1:length(R)
        if ~isempty(R{i})
            kidney_layer_index = 0;
            cancer_layer_index = 0;
-           
+           is_layer_counted = false;
+
            for j = 1:length(R{i})
                if ~isempty(R{i}{j})
+                   if(~is_layer_counted)
+                        layer_number = layer_number + 1;
+                        is_layer_counted = true;
+                   end
+
                    for k=1:(length(R{i}{j})-1)
                        % nerka
                        if ~isempty(R{i}{j}{k}) && strcmp(R{i}{j}{k+1},'nerka')
                            result = R{i}{j}{k};
-                           new_output_file_name = sprintf('%s_kidney_%d-%d.ctl', output_file_name_base, i, kidney_layer_index); 
+                           new_output_file_name = sprintf('%s_kidney_%d-%d.ctl', output_file_name_base, layer_number, kidney_layer_index);
                            fprintf('Adding R{%d}{%d}{%d} to file %s\n', i, j, k, strcat('output/',new_output_file_name));
                            output_file = fopen(strcat('output/',new_output_file_name), 'w');
                            fprintf(output_file, rleCompressLayer(result, new_output_file_name));
                            kidney_layer_index = kidney_layer_index + 1;
                        end
-                       
+
                        % rak
                        if ~isempty(R{i}{j}{k}) && strcmp(R{i}{j}{k+1},'Cancer')
                            result = R{i}{j}{k};
-                           new_output_file_name = sprintf('%s_cancer_%d-%d.ctl', output_file_name_base, i, cancer_layer_index); 
+                           new_output_file_name = sprintf('%s_cancer_%d-%d.ctl', output_file_name_base, layer_number, cancer_layer_index);
                            fprintf('Adding R{%d}{%d}{%d} to file %s\n', i, j, k, strcat('output/',new_output_file_name));
                            output_file = fopen(strcat('output/',new_output_file_name), 'w');
                            fprintf(output_file, rleCompressLayer(result, new_output_file_name));
                            cancer_layer_index = cancer_layer_index + 1;
                        end
                    end
-                   
+
                end
            end
-           
-       end    
+
+       end
     end
 end
